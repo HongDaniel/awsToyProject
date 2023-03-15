e67d17a32fa5d6e57b6e806e5a0e1fa54a14e0c4
CORP\84922 <p.daniel.jung@kalmate.net>
2023-03-15 13:57:14 +0900

    Initial commit

---
+++ src/main/java/com/danny/awstoyprojectfinal/AwsToyProjectFinalApplication.java
@@ -0,0 +1,13 @@
+package com.danny.awstoyprojectfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsToyProjectFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsToyProjectFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danny/awstoyprojectfinal/AwsToyProjectFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danny.awstoyprojectfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsToyProjectFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
